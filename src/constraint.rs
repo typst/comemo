b75fad171382a2edcf981edb83e9c042d5f4f311
Daniel Csillag <dccsillag@gmail.com>
2023-04-15 10:38:51 -0300

    Change SipHash2-4 -> SipHash1-3 (#1)

--- src/constraint.rs
+++ src/constraint.rs
@@ -1,7 +1,7 @@
 use std::cell::RefCell;
 use std::hash::Hash;
 
-use siphasher::sip128::{Hasher128, SipHasher};
+use siphasher::sip128::{Hasher128, SipHasher13};
 
 use crate::track::Trackable;
 
@@ -121,7 +121,7 @@ where
 /// Produce a 128-bit hash of a value.
 #[inline]
 pub fn hash<T: Hash>(value: &T) -> u128 {
-    let mut state = SipHasher::new();
+    let mut state = SipHasher13::new();
     value.hash(&mut state);
     state.finish128().as_u128()
 }
